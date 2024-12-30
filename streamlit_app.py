f0e412888c3398b9c3b320aa83dce0810ca3cf15
divya-shree1125 <sriyanka1120@gmail.com>
2024-12-30 23:20:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error,r2_score,mean_squared_error
 from sklearn.metrics import confusion_matrix
-import os
-import json
+# import os
+# import json
 # firebase_credentials=json.loads(os.getenv("FIREBASE_CREDENTIALS"))
 
 # # Initialize Firebase Admin SDK
